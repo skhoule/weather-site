50f23ef48572d10666a3d4dfd3162cefb6f44344
Sal Khoule <s.khoule@gmail.com>
2019-04-15 13:40:36 -0400

    Add high and low to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (latitude, longtitude, callback) => {
         } else if (body.error) {
             callback("Unable to find loction.", undefined)
         } else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + '. There is a ' + body.currently.precipProbability +'% chance of rain.')
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' with a high of  '+ body.daily.data[0].temperatureHigh +' and a low of ' + body.daily.data[0].temperatureLow + ' . There is a ' + body.currently.precipProbability + '% chance of rain.')
         }
     })
 }
